cc344695cea954cf627cd90fd2f64438e8465ca5
jatinupadhayay <jatinupadhayay1204@gmail.com>
2024-09-05 20:51:54 +0530

    Updates

---
+++ precache-manifest.c6d7606ddfc4e9d9d436f8e4b85991e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d23d158722901686d06a778512709a",
+    "url": "/ecommerceproduct/index.html"
+  },
+  {
+    "revision": "b7dc55ff4eb8b492df61",
+    "url": "/ecommerceproduct/static/css/main.4cf378dc.chunk.css"
+  },
+  {
+    "revision": "c2d144b04701214df3e8",
+    "url": "/ecommerceproduct/static/js/2.7dec5cce.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/ecommerceproduct/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "b7dc55ff4eb8b492df61",
+    "url": "/ecommerceproduct/static/js/main.4e0ee2a2.chunk.js"
+  },
+  {
+    "revision": "cf985e588ed054b310cd",
+    "url": "/ecommerceproduct/static/js/runtime~main.655c76cc.js"
+  },
+  {
+    "revision": "4a387bda853bca3782d73234c786a150",
+    "url": "/ecommerceproduct/static/media/cart.4a387bda.png"
+  },
+  {
+    "revision": "2e4cca8fdf9ba2412f1257eb3b0fad24",
+    "url": "/ecommerceproduct/static/media/logo.2e4cca8f.png"
+  },
+  {
+    "revision": "d5d01a734d75036c1a1629da634de011",
+    "url": "/ecommerceproduct/static/media/threedot.d5d01a73.png"
+  }
+]);
